9b7703dbf5d986a380adff63cc1051af23f39292
Gaëtan H <gaetanhus@gmail.com>
2024-04-14 01:37:49 +0200

    Update font to Nunito and default URL in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 import type { Component } from "@/components/utils/component";
 import type { PropsWithChildren } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "Simplist - The simplest way to integrate a blog into your website",
   description: "Simplist is a simple, fast. Post your content in the simplest way possible, and just get your content back with an API."
 };
@@ -14,7 +19,7 @@ export const metadata: Metadata = {
 const Layout: Component<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 };
