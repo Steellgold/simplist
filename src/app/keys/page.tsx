7f60ee7b40843c4f1a148c5b01b4eff5e8ed031c
Gaëtan H <gaetanhus@gmail.com>
2024-04-14 14:00:25 +0200

    Add new page components for API keys, products, and posts

--- src/app/keys/page.tsx
+++ src/app/keys/page.tsx
@@ -5,14 +5,13 @@ import { PageLayout } from "@/components/page.layout";
 
 const Home = (): ReactElement => {
   return (
-    <PageLayout title="Dashboard">
+    <PageLayout title="API Keys">
       <div className="flex flex-col items-center gap-1 text-center">
         <h3 className="text-2xl font-bold tracking-tight">
-          You have no products
+          You have no API keys
         </h3>
-        <p className="text-sm text-muted-foreground">You can start selling as soon as you add a product.
-        </p>
-        <Button className="mt-4">Add Product</Button>
+        <p className="text-sm text-muted-foreground">You can start generating API keys as soon as you add one.</p>
+        <Button className="mt-4">Add API Key</Button>
       </div>
     </PageLayout>
   );
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -5,14 +5,13 @@ import { PageLayout } from "@/components/page.layout";
 
 const Home = (): ReactElement => {
   return (
-    <PageLayout title="Dashboard">
+    <PageLayout title="Posts">
       <div className="flex flex-col items-center gap-1 text-center">
         <h3 className="text-2xl font-bold tracking-tight">
-          You have no products
+          You have no posts
         </h3>
-        <p className="text-sm text-muted-foreground">You can start selling as soon as you add a product.
-        </p>
-        <Button className="mt-4">Add Product</Button>
+        <p className="text-sm text-muted-foreground">You can start writing as soon as you add a post.</p>
+        <Button className="mt-4">Add Post</Button>
       </div>
     </PageLayout>
   );
