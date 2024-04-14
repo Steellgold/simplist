ef92dfa2116bae22a4ce3477a65cf08316779186
Gaëtan H <gaetanhus@gmail.com>
2024-04-14 16:51:58 +0200

    Add hiddenX prop to DialogContent component for conditional visibility

--- src/lib/components/ui/dialog.tsx
+++ src/lib/components/ui/dialog.tsx
@@ -32,7 +32,8 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
-    black?: boolean
+    black?: boolean,
+    hiddenX?: boolean
   }
 >(({ className, children, black, ...props }, ref) => (
   <DialogPortal>
@@ -47,7 +48,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className={cn(
-        "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground", { "text-black": black}
+        "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground", { "text-black": black, "hidden": props.hiddenX }
       )}>
         <Cross2Icon className="h-4 w-4" />
         <span className="sr-only">Close</span>
