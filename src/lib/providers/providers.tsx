86faae2eae06fff488c4f18ca89ad81755e44531
Gaëtan H <gaetanhus@gmail.com>
2024-04-14 19:01:59 +0200

    Add QueryProvider component for managing React Query client

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -2,13 +2,16 @@ import type { PropsWithChildren } from "react";
 import { ThemeProvider } from "./theme";
 import type { Component } from "@/components/utils/component";
 import { Toaster } from "@/components/ui/toaster";
+import { QueryProvider } from "./query-provider";
 
 export const Providers: Component<PropsWithChildren> = ({ children }) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-      <Toaster />
+    <QueryProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <Toaster />
 
-      {children}
-    </ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </QueryProvider>
   );
 };
