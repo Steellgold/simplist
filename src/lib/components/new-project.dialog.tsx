a9565dda767a6b300bf8f8889345554263e0191e
Gaëtan H <gaetanhus@gmail.com>
2024-04-14 19:46:00 +0200

    Refactor NewProjectDialog component to use ProjectSchema and useForm from react-hook-form

--- src/lib/components/new-project.dialog.tsx
+++ src/lib/components/new-project.dialog.tsx
@@ -5,17 +5,18 @@ import { Dialog, DialogContent, DialogFooter, DialogTrigger } from "./ui/dialog"
 import type { Component } from "./utils/component";
 import { useState, useTransition, type PropsWithChildren } from "react";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
-import { useForm } from "react-hook-form";
-import { ProjectSchema, createProject } from "@/actions/projects";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type { z } from "zod";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useForm } from "react-hook-form";
+import { ProjectSchema } from "@/schemas/project";
+import { createProject } from "@/actions/projects";
 
 type NewProjectDialogProps = { isFirst: boolean } & PropsWithChildren;
 
 export const NewProjectDialog: Component<NewProjectDialogProps> = ({ isFirst, children }) => {
-  const [error, setError] = useState<{ title: string; message: string; isSuccess: boolean }>({ title: "", message: "", isSuccess: false });
+  const [_, setError] = useState<{ title: string; message: string; isSuccess: boolean }>({ title: "", message: "", isSuccess: false });
   const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof ProjectSchema>>({
@@ -26,13 +27,11 @@ export const NewProjectDialog: Component<NewProjectDialogProps> = ({ isFirst, ch
   const onSubmit = (values: z.infer<typeof ProjectSchema>): void => {
     setError({ title: "", message: "", isSuccess: false });
 
-    console.log("bbb", values);
     startTransition(() => {
       void createProject(values)
         .then((data) => {
           console.log(data);
-        })
-        .catch(() => setError({ title: "Error", message: "An error occurred!", isSuccess: false }));
+        });
     });
   };
 
@@ -42,20 +41,14 @@ export const NewProjectDialog: Component<NewProjectDialogProps> = ({ isFirst, ch
         {children}
       </DialogTrigger>
 
-      <DialogContent className="max-w-xl p-0 overflow-hidden" black hiddenX>
-        <div className="p-5">
-          <h2 className="text-2xl font-semibold">Create your {isFirst ? "first " : "next "}project</h2>
-          <p className="text-muted-foreground mt-2">
-            {isFirst ? "Create your first project and start posting content." : "Create your next project and start posting content."}
-          </p>
-
-          {error.title && error.message && (
-            <p className={`mt-2 text-${error.isSuccess ? "green-500" : "red-500"}`}>
-              {error.title}: {error.message}
+      <Form {...form}>
+        <DialogContent className="max-w-xl p-0 overflow-hidden" black hiddenX>
+          <div className="p-5">
+            <h2 className="text-2xl font-semibold">Create your {isFirst ? "first " : "next "}project</h2>
+            <p className="text-muted-foreground mt-2">
+              {isFirst ? "Create your first project and start posting content." : "Create your next project and start posting content."}
             </p>
-          )}
 
-          <Form {...form}>
             {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
             <form onSubmit={form.handleSubmit(onSubmit)}>
               <FormField control={form.control} name="name" render={({ field }) => (
@@ -73,14 +66,14 @@ export const NewProjectDialog: Component<NewProjectDialogProps> = ({ isFirst, ch
               )} />
 
               <DialogFooter className="mt-3">
-                <Button variant={"default"} disabled={isPending} size={"sm"}>
+                <Button variant={"default"} disabled={isPending} type="submit">
                   Create Project
                 </Button>
               </DialogFooter>
             </form>
-          </Form>
-        </div>
-      </DialogContent>
+          </div>
+        </DialogContent>
+      </Form>
     </Dialog>
   );
 };
