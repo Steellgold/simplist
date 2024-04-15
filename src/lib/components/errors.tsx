e7e92be0445ac10c412ab8b9f1ab103821358df2
Gaëtan H <gaetanhus@gmail.com>
2024-04-15 22:30:43 +0200

    Add prosp to NotFound, AccessDenied, and NotLogged components to replace "page" by other word

--- src/lib/components/errors.tsx
+++ src/lib/components/errors.tsx
@@ -1,23 +1,30 @@
 import type { ReactElement } from "react";
+import type { Component } from "./utils/component";
 
-export const NotFound = (): ReactElement => {
+type Props = {
+  replacePageTo?: string;
+};
+
+export const NotFound: Component<Props> = ({ replacePageTo }) => {
   return (
     <div className="flex flex-col items-center gap-1 text-center">
       <h3 className="text-2xl font-bold tracking-tight">
-        Page not found
+        {replacePageTo ? `${replacePageTo.charAt(0).toUpperCase()}${replacePageTo.slice(1)} not found` : "Page not found"}
       </h3>
-      <p className="text-sm text-muted-foreground">The page you are looking for does not exist.</p>
+      <p className="text-sm text-muted-foreground">
+        The {replacePageTo ? replacePageTo : "page"} you are looking for does not exist.
+      </p>
     </div>
   );
 };
 
-export const AccessDenied = (): ReactElement => {
+export const AccessDenied: Component<Props> = ({ replacePageTo }) => {
   return (
     <div className="flex flex-col items-center gap-1 text-center">
       <h3 className="text-2xl font-bold tracking-tight">
         Access denied
       </h3>
-      <p className="text-sm text-muted-foreground">You do not have permission to view this page.</p>
+      <p className="text-sm text-muted-foreground">You do not have permission to view this {replacePageTo ? replacePageTo : "page"}.</p>
     </div>
   );
 };
@@ -33,13 +40,13 @@ export const ServerError = (): ReactElement => {
   );
 };
 
-export const NotLogged = (): ReactElement => {
+export const NotLogged: Component<Props> = ({ replacePageTo }) => {
   return (
     <div className="flex flex-col items-center gap-1 text-center">
       <h3 className="text-2xl font-bold tracking-tight">
         You are not logged in
       </h3>
-      <p className="text-sm text-muted-foreground">You need to be logged in to view this page.</p>
+      <p className="text-sm text-muted-foreground">You need to be logged in to view this {replacePageTo ? replacePageTo : "page"}.</p>
     </div>
   );
 };
