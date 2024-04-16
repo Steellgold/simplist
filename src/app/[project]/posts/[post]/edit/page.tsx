3161f54a6bd88f6d098166e48c9468279b4ae7b9
Gaëtan H <gaetanhus@gmail.com>
2024-04-16 10:43:50 +0200

    Refactor page.tsx and route.ts components for post editing functionality

--- src/app/[project]/posts/[post]/edit/page.tsx
+++ src/app/[project]/posts/[post]/edit/page.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button";
 import { PageLayout } from "@/components/page.layout";
 import type { AsyncComponent } from "@/components/utils/component";
 import { db } from "@/utils/db/prisma";
-import { NotebookPen } from "lucide-react";
+import { Undo2 } from "lucide-react";
 import { NotFound } from "@/components/errors";
+import Link from "next/link";
 
 type PageProps = {
   params: {
@@ -26,10 +27,14 @@ const Post: AsyncComponent<PageProps> = async({ params }) => {
       description={post ? post.content.slice(0, 100) : "Review your post and make changes by clicking on the button below."}
       projectId={project}
       actions={post ? (
-        <Button>
-          <NotebookPen size={16} className="mr-2" />
-          Edit Post
-        </Button>
+        <>
+          <Button asChild>
+            <Link href={`/${project}/posts/${postId}/edit`}>
+              <Undo2 size={16} className="mr-2" />
+              Back to posts
+            </Link>
+          </Button>
+        </>
       ) : <></>}
     >
       {post ? (<p>{post.content}</p>) : <NotFound replacePageTo="post" />}
