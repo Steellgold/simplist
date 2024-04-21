f2d924f3e7e7fc189876db7fdfa6f2670233a401
Gaëtan H <gaetanhus@gmail.com>
2024-04-21 18:20:48 +0200

    Edit logging for API calls and update key last used timestamp

--- src/app/api/utils.ts
+++ src/app/api/utils.ts
@@ -1,4 +1,7 @@
+import { dayJS } from "@/dayjs/day-js";
 import { env } from "@/env.mjs";
+import type { KeyData } from "@/types";
+import { redis } from "@/utils/db/upstash";
 
 type LogCallProps = {
   key: string;
@@ -10,8 +13,8 @@ type LogCallProps = {
   status: number;
 };
 
-export const logCall = async({ key, projectId, slug, postId, ip, method, status }: LogCallProps): Promise<void> => {
-  await fetch("https://api.tinybird.co/v0/events?name=api_calls", {
+export const logCall = ({ key, projectId, slug, postId, ip, method, status }: LogCallProps): void => {
+  void fetch("https://api.tinybird.co/v0/events?name=api_calls", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
@@ -28,4 +31,17 @@ export const logCall = async({ key, projectId, slug, postId, ip, method, status 
       status
     })
   });
+
+  if (key) void setKeyLU(key);
+};
+
+export const setKeyLU = async(key: string): Promise<void> => {
+  const keyData = await redis.get(`api_key:${key}`) as KeyData;
+  await redis.set(`api_key:${key}`, { ...keyData, lastUsed: new Date().toISOString() });
+};
+
+export const getKeyLU = async(key: string): Promise<string> => {
+  const keyData = await redis.get(`api_key:${key}`) as KeyData;
+  if (!keyData.lastUsed) return "Never";
+  return dayJS(keyData.lastUsed).format("YYYY-MM-DD HH:mm");
 };
