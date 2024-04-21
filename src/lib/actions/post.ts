25e7d6c7f41962eb9e5a7a4c317cb731ca7aafb8
Gaëtan H <gaetanhus@gmail.com>
2024-04-21 11:39:21 +0200

    Refactor post update logic and add support for metadata in updatePost function

--- src/lib/actions/post.ts
+++ src/lib/actions/post.ts
@@ -70,3 +70,73 @@ export const createPost = async(values: z.infer<typeof PostSchema>): Promise<Pos
   revalidatePath(`/${project.id}/posts`);
   throw redirect(`/${project.id}/posts`);
 };
+
+export const updatePost = async(id: string, values: z.infer<typeof PostSchema>): Promise<Post | {
+  title: string;
+  message: string;
+}> => {
+  const validatedFields = PostSchema.safeParse(values);
+
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user) return { title: "Error", message: "An error occurred!" };
+  if (!validatedFields.success) return { title: "Invalid Fields", message: "Invalid fields provided!" };
+
+  const { content, excerpt, title, status, banner, metadata, lang } = validatedFields.data;
+
+  const post = await db.post.findFirst({ where: { id } });
+  if (!post) return { title: "Invalid Data", message: "Invalid post ID provided. Refresh the page and try again." };
+
+  const data = await db.post.update({
+    where: { id },
+    data: {
+      content,
+      excerpt,
+      status,
+      title,
+      banner,
+      lang: lang.toUpperCase() as Lang
+    }
+  });
+
+  const oldMetadata = await db.meta.findMany({ where: { postId: id } });
+  // on supprime ceux qui ne sont plus présents (dans metadata) par rapport à oldMetadata
+  const metadataToDelete = oldMetadata.filter((meta) => !metadata?.some((m) => m.key == meta.key));
+  const metadataToAdd = metadata?.filter((meta) => !oldMetadata.some((m) => m.key == meta.key));
+
+  if (metadataToDelete.length) {
+    await db.meta.deleteMany({ where: { id: { in: metadataToDelete.map((meta) => meta.id) } } });
+  }
+
+  if (metadataToAdd?.length) {
+    // @ts-ignore: createdAt, updatedAt, and id are not present (normal, as we're creating a new record here)
+    const metadataData = metadataToAdd.map((meta: Meta) => ({
+      ...meta,
+      postId: id,
+      type: meta.type == "STRING" ? "STRING" : meta.type == "BOOLEAN" ? "BOOLEAN" : "NUMBER"
+    }));
+    // @ts-ignore
+    await db.meta.createMany({ data: metadataData });
+  }
+
+
+  await redis.set(`post:${data.slug}`, {
+    ...data,
+    metadata: metadata || []
+  });
+
+  revalidatePath(`/${post.projectId}/posts`);
+  throw redirect(`/${post.projectId}/posts`);
+};
+
+export const deletePost = async(id: string): Promise<void> => {
+  const post = await db.post.findFirst({ where: { id } });
+  if (!post) return;
+
+  await db.post.delete({ where: { id } });
+  await redis.del(`post:${post.slug}`);
+
+  revalidatePath(`/${post.projectId}/posts`);
+  throw redirect(`/${post.projectId}/posts`);
+};
--- src/lib/schemas/post.ts
+++ src/lib/schemas/post.ts
@@ -11,6 +11,7 @@ export const PostSchema = z.object({
     .min(3, { message: "Content must be at least 3 characters long" })
     .max(10000, { message: "Content must be at most 10000 characters long" }),
   metadata: z.array(z.object({
+    id: z.string().optional().nullable(),
     key: z.string()
       .min(3, { message: "Key must be at least 3 characters long" })
       .max(255, { message: "Key must be at most 255 characters long" }),
@@ -21,7 +22,8 @@ export const PostSchema = z.object({
       z.boolean(),
       z.number(),
       z.string()
-    ])
+    ]),
+    postId: z.string().optional().nullable()
   })).optional().nullable(),
   banner: z.string().optional().nullable(),
   status: z.enum(["DRAFT", "PUBLISHED"]),
