5e3d23bb9903b4ab54f1e450aac52d4a4835af54
Gaëtan H <gaetanhus@gmail.com>
2024-05-05 19:54:16 +0200

    Refactor slugify function to remove random string generation

--- src/lib/slugify.ts
+++ src/lib/slugify.ts
@@ -1,13 +1,9 @@
-export const slugify = (title: string, withRandom: boolean = true): string => {
-  let slug = title
+export const slugify = (title: string): string => {
+  return title
     .toLowerCase()
     .replace(/ /g, "-")
     .replace(/[^\w-]+/g, "")
     .replace(/--+/g, "-")
     .replace(/^-+/, "")
     .replace(/-+$/, "");
-  if (withRandom) {
-    slug += "-" + Math.random().toString(36).substring(7);
-  }
-  return slug;
 };
