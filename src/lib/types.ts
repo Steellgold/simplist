29c964912ffaf39143d5e8faa0880b0043d6660b
Gaëtan H <gaetanhus@gmail.com>
2024-05-06 01:13:50 +0200

    Refactor types.ts to include PostData type and import Meta from @prisma/client

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,30 @@
+import type { Meta } from "@prisma/client";
+
 export type KeyData = {
   id: string;
   projectId: string;
   authorId: string;
   status: "ACTIVE" | "INACTIVE";
   lastUsed?: string;
+}
+
+export type PostData = {
+  authorId: string;
+  banner: string | null;
+  content: string;
+  createdAt: string;
+  excerpt: string;
+  id: string;
+  metadata: Meta[];
+  projectId: string;
+  slug: string;
+  status: string;
+  title: string;
+  updatedAt: string;
+  calls: {
+    [key: string]: {
+      count: number;
+      lastCall: string;
+    };
+  };
 }
