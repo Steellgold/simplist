a8701c9c7f6829b3e5f65452366a47d893be38b3
Gaëtan H <gaetanhus@gmail.com>
2024-05-06 01:05:53 +0200

    Add egg utility module with super secret phrase and default content

---
+++ src/lib/utils/egg.ts
@@ -0,0 +1,6 @@
+export const superSecretPhraseToUnlockConfetti = "Build in a weekend";
+// eslint-disable-next-line max-len
+export const superSecretImageBannerURL = "https://github.com/supabase/supabase/blob/master/apps/www/public/images/blog/oss-hackathon/thumbnail.png?raw=true";
+
+export const defaultContent = `# Hello, world!
+> This is a post content example.`;
