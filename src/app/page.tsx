c6647e393d0e9d6878ea7ec0c065b5c57731b216
Gaëtan H <gaetanhus@gmail.com>
2024-05-06 20:59:19 +0200

    Refactor project page to display project cards with project name and number of posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,107 @@
-import type { ReactElement } from "react";
+"use client";
 
-import { Button } from "@/components/ui/button";
-import { PageLayout } from "@/components/page.layout";
-import { db } from "@/utils/db/prisma";
-import { createClient } from "@/utils/supabase/server";
-import { NewProjectDialog } from "@/components/new-project.dialog";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { dayJS } from "@/dayjs/day-js";
-import type { Component } from "@/components/utils/component";
+import { useEffect, useState, type ReactElement } from "react";
+import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
-import { Plus } from "lucide-react";
+import { z } from "zod";
+import { toast } from "sonner";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CustomCard } from "@/components/ui/custom-card";
+import { ChevronRight, Plus } from "lucide-react";
+import { NewProjectDialog } from "@/components/new-project.dialog";
+import { Button } from "@/components/ui/button";
 
-type NoProjectProps = {
-  isFirst?: boolean;
+type Project = {
+  id: string;
+  name: string;
+  createdAt: string;
+  posts: {
+    projectId: string;
+  }[];
 };
 
-const NoProject: Component<NoProjectProps> = ({ isFirst = true }) => {
-  return (
-    <PageLayout title="Projects">
-      <div className="flex flex-col items-center gap-1 text-center">
-        <h3 className="text-2xl font-bold tracking-tight">
-          You have no projects
-        </h3>
-        <p className="text-sm text-muted-foreground">You can start creating as soon as you add a project.</p>
-        <NewProjectDialog isFirst={isFirst}>
-          <Button>Create your first project</Button>
+const Home = (): ReactElement => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const fetchProjects = async(): Promise<void> => {
+      const response = await fetch("/api/user/projects");
+
+      const schema = z.object({
+        projects: z.array(z.object({
+          id: z.string(),
+          name: z.string(),
+          createdAt: z.string(),
+          posts: z.array(z.object({
+            projectId: z.string()
+          }))
+        }))
+      }).safeParse(await response.json());
+
+      if (!schema.success) {
+        console.error(schema.error);
+        toast.error("Failed to fetch projects");
+        return;
+      }
+
+      setProjects(schema.data.projects);
+      setIsLoading(false);
+    };
+
+    void fetchProjects();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 p-3">
+        <Skeleton className="w-full h-40" />
+        <Skeleton className="w-full h-40" />
+        <Skeleton className="w-full h-40" />
+      </div>
+    );
+  }
+
+  if (!projects.length) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-2xl font-bold">You have no projects</h1>
+        <p className="text-lg text-muted-foreground">To start creating posts, begin by creating a project</p>
+
+        <div className="my-2"></div>
+
+        <NewProjectDialog isFirst>
+          <Button>
+            <Plus className="h-4" />Create a project
+          </Button>
         </NewProjectDialog>
       </div>
-    </PageLayout>
-  );
-};
-
-const Home = async(): Promise<ReactElement> => {
-  const supabase = createClient();
-  const { data: { user } } = await supabase.auth.getUser();
-  if  (!user) return <NoProject />;
-
-  const projects = await db.project.findMany({
-    where: { userId: user.id }
-  });
-
-  if (!projects) return <NoProject isFirst />;
-  if (projects.length === 0) return <NoProject isFirst />;
+    );
+  }
 
   return (
-    <PageLayout title="Projects" center={false} actions={(
-      <NewProjectDialog isFirst={projects.length === 0}>
-        <Button>
-          <Plus className="h-4 w-4" />&nbsp;
-          Project
-        </Button>
-      </NewProjectDialog>
-    )}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
-        {projects.map((project) => (
-          <Card key={project.id} className="w-full border-2 hover:cursor-pointer hover:border-primary/40 transition-colors">
-            <Link href={`/${project.id}/posts`}>
-              <CardHeader>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 p-3">
+      {projects.map((project) => (
+        <CustomCard key={project.id} className="w-full h-36 border-2 hover:cursor-pointer transition-colors group">
+          <Link href={`/${project.id}/posts`}>
+            <CardHeader className="flex flex-row justify-between">
+              <div className="flex flex-col justify-center">
                 <CardTitle>{project.name}</CardTitle>
-                <CardDescription>Created on {dayJS(project.createdAt).format("MMMM DD, YYYY")}</CardDescription>
-              </CardHeader>
-            </Link>
-          </Card>
-        ))}
-      </div>
-    </PageLayout>
+                <CardDescription>
+                  {project.posts.length} post{project.posts.length === 1 ? "" : "s"} in this project
+                </CardDescription>
+              </div>
+
+              <ChevronRight
+                size={24}
+                className="text-[#252525] group-hover:text-[#5c5c5c] transition-all transform group-hover:translate-x-1" />
+            </CardHeader>
+          </Link>
+        </CustomCard>
+      ))}
+    </div>
   );
 };
 
