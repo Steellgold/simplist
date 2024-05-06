489d9f817028f6711677662562f836c917726f17
Gaëtan H <gaetanhus@gmail.com>
2024-05-06 17:49:46 +0200

    Remove unused files and code

--- src/app/[project]/posts/[post]/route.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import type { NextRequest, NextResponse } from "next/server";
-
-export const GET = ({ url }: NextRequest): NextResponse => {
-  const params = url.split("/").slice(3, 5);
-  if (!params[0] || !params[1]) return redirect("/");
-  return redirect(`/${params[0]}/posts/`);
-};
