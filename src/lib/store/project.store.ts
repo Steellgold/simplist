cdf7dd5a90456be8c6bb3ddf86b47451246e6d28
Gaëtan H <gaetanhus@gmail.com>
2024-05-08 23:13:58 +0200

    Refactor project store and add persistence

--- src/lib/store/project.store.ts
+++ src/lib/store/project.store.ts
@@ -1,16 +1,36 @@
-import type { Project } from "@prisma/client";
-import { create } from "zustand";
+// import { create } from "zustand";
 
-type ProjectStoreType = {
-  activeProject: Project | null;
-  list: Project[];
-  setProject: (project: Project) => void;
-  setProjects: (projects: Project[]) => void;
+type Project = {
+  id: string;
+  name: string;
 }
 
-export const useProjectStore = create<ProjectStoreType>((set) => ({
-  activeProject: null,
-  list: [],
-  setProject: (project) => set({ activeProject: project }),
-  setProjects: (projects) => set({ list: projects })
-}));
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type ProjectsStore = {
+  projects: Project[];
+  active: Project | null;
+  setProjects: (projects: Project[]) => void;
+  setActive: (projectId: string) => void;
+
+  isSwitching: boolean;
+  setIsSwitching: (isSwitching: boolean) => void;
+  setSwitched: () => void;
+};
+
+export const useProjectStore = create(
+  persist<ProjectsStore>(
+    (set) => ({
+      projects: [],
+      active: null,
+      isSwitching: false,
+      setProjects: (projects) => set({ projects }),
+      setActive: (active) => set((state) => ({ active: state.projects.find((project) => project.id === active) })),
+
+      setIsSwitching: (isSwitching) => set({ isSwitching }),
+      setSwitched: () => set((state) => ({ isSwitching: !state.isSwitching }))
+    }),
+    { name: "projects-store" }
+  ),
+);
