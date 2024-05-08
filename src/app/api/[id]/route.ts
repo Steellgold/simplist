01d72e767ba5d5f2665a3200d3adca63a7e141d9
Gaëtan H <gaetanhus@gmail.com>
2024-05-08 21:41:28 +0200

    Add IPAPI integration to GET route for fetching post

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -1,8 +1,11 @@
 /* eslint-disable camelcase */
-import { env } from "@/env.mjs";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { db } from "@/utils/db/prisma";
+import type { Variant } from "@prisma/client";
+import { Lang, type APIKey, type Post, type Project } from "@prisma/client";
 import type { IPAPIResponse } from "../ipapi.type";
+import { env } from "@/env.mjs";
 
 type Request = {
   params: {
@@ -10,15 +13,57 @@ type Request = {
   };
 };
 
-export const GET = async({ headers }: NextRequest, { params }: Request): Promise<NextResponse> => {
-  const slug = params.id;
-  if (!slug) return NextResponse.json({ message: "Not Found" }, { status: 404 });
+export const GET = async({ headers, url, nextUrl }: NextRequest, { params }: Request): Promise<NextResponse> => {
+  const postId = params.id;
+  if (!postId) return NextResponse.json({ message: "Not Found" }, { status: 404 });
 
-  const xApiKey = headers.get("x-api-key");
-  if (!xApiKey) return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  const key = headers.get("x-api-key");
+  if (!key) return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+  const keyData = await db.$queryRaw<APIKey[]>`SELECT * FROM "APIKey" WHERE "key" = ${key} AND "status" = 'ACTIVE' LIMIT 1`;
+  if (keyData.length == 0) return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  const activeKey = keyData[0];
+
+  const project = await db.$queryRaw<Project[]>`SELECT * FROM "Project" WHERE "id" = ${activeKey.projectId} LIMIT 1`;
+  if (project.length == 0) return NextResponse.json({ message: "Not Found" }, { status: 404 });
+  const activeProject = project[0];
+
+  const post = await db.$queryRaw<Post[]>`SELECT * FROM "Post" WHERE "id" = ${postId} AND "projectId" = ${activeProject.id}`;
+  if (post.length == 0) return NextResponse.json({ message: "Not Found" }, { status: 404 });
+
+  const activePost: Post = post[0];
+  const variants: Variant[] = [];
+
+  const spVariants = nextUrl.searchParams.get("variants");
+  if (spVariants) {
+    const tempVariants = await db.$queryRaw<Variant[]>`SELECT * FROM "Variant" WHERE "parentId" = ${activePost.id} LIMIT 10`;
+    console.log(tempVariants);
+    console.log(`Found ${tempVariants.length} variant(s) for post ${activePost.id}`);
+
+    switch (spVariants) {
+      case "true":
+        console.log("User want to get all variants");
+        variants.push(...tempVariants);
+        break;
+      default:
+        console.log("User want to get specific(s) variant(s)", spVariants);
+
+        for (const v of spVariants.split(",")) {
+          if (!v) continue;
+          if (Object.values(Lang).includes(v as Lang)) {
+            const variant = tempVariants.find((x) => x.lang == v);
+            if (variant) variants.push(variant);
+          } else {
+            console.log(`Invalid variant lang: ${v}`);
+          }
+        }
+        break;
+    }
+  }
 
   let local: IPAPIResponse | undefined = undefined;
   const ipAddr = headers.get("X-User-IP");
+
   if (ipAddr) {
     if (ipAddr.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/)) {
       local = await fetch(`https://ipapi.co/${ipAddr}/json/`).then((res) => res.json() as Promise<IPAPIResponse>);
@@ -35,7 +80,7 @@ export const GET = async({ headers }: NextRequest, { params }: Request): Promise
     },
     body: JSON.stringify({
       projectId: "events_example",
-      postId: slug,
+      postId,
       createdAt: new Date().toISOString(),
       ip: local?.ip || ipAddr,
       city: local?.city || "Unknown",
@@ -49,5 +94,10 @@ export const GET = async({ headers }: NextRequest, { params }: Request): Promise
     })
   });
 
-  return NextResponse.json({ message: "Hello, World!" });
+  return NextResponse.json({
+    post: {
+      ...activePost,
+      variants: spVariants ? variants : undefined
+    }
+  });
 };
