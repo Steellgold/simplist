f7bcbfbb10f4629af069a12ca9aee4cff1c4637a
Gaëtan H <gaetanhus@gmail.com>
2024-05-09 01:45:50 +0200

    chore: Update sidebar header component to improve loading state handling

--- src/lib/components/sidebar.header.tsx
+++ src/lib/components/sidebar.header.tsx
@@ -1,16 +1,29 @@
 "use client";
 
 import Link from "next/link";
-import type { ReactElement } from "react";
+import { useEffect, useState, type ReactElement } from "react";
 import { useProjectStore } from "@/store/project.store";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { Skeleton } from "./ui/skeleton";
 
 export const SidebarHeader = (): ReactElement => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const { active, projects, setActive } = useProjectStore();
   const url = usePathname();
   const router = useRouter();
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) return (
+    <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 select-none">
+      <Skeleton className="w-full h-6" />
+    </div>
+  );
 
   if (!active || url === "/") return (
     <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 select-none">
@@ -23,7 +36,7 @@ export const SidebarHeader = (): ReactElement => {
   return (
     <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 select-none">
       <Link href="/">
-        <Image src="/_static/logos/simple-light.png" alt="Logo" width={20} height={17.81} />
+        <Image src="/_static/logos/simple-light.png" alt="Logo" width={14.38} height={20.02} />
       </Link>
 
       <span className="text-muted-foreground/80 mx-2 lg:mx-4 select-none">/</span>
