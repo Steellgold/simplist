60915e46fcaa09b0e567127271a5f61d4dbff8f7
Gaëtan H <gaetanhus@gmail.com>
2024-05-09 03:15:30 +0200

    Refactor utils.ts to add getRandomData function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { dayJS } from "./dayjs/day-js";
 
 export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs));
@@ -7,4 +8,15 @@ export const cn = (...inputs: ClassValue[]): string => {
 
 export const hideKey = (key: string): string => {
   return `${key.slice(0, 4)}${"*".repeat(key.length - 2)}`;
+};
+
+export const getRandomData = (): { date: string; requests: number }[] => {
+  const data = [];
+  for (let i = 0; i < 10; i++) {
+    data.unshift({
+      date: dayJS().subtract(i, "day").format("MMMM D, YYYY"),
+      requests: Math.floor(Math.random() * 10000)
+    });
+  }
+  return data;
 };
