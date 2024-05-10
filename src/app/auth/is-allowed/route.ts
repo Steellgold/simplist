981aa7b0170b77e70ca194ec59f37c5f32b64641
Gaëtan H <steellgold0@gmail.com>
2024-05-10 15:13:09 +0200

    Refactor GET route to allow passing userId as a query parameter

--- src/app/auth/is-allowed/route.ts
+++ src/app/auth/is-allowed/route.ts
@@ -5,19 +5,23 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export const GET = async({ nextUrl }: NextRequest): Promise<NextResponse> => {
-  const supabase = createClient();
-  const { data: { user }, error } = await supabase.auth.getUser();
-  if (!user || error) return NextResponse.json({ isAllowed: false });
-
   const params = new URL(nextUrl).searchParams;
   const projectId = params.get("projectId");
+  let userId: string = params.get("userId") || "";
+
   if (!projectId) return NextResponse.json({ isAllowed: false });
+  if (!userId) {
+    const supabase = createClient();
+    const { data: { user }, error } = await supabase.auth.getUser();
+    if (!user || error) return NextResponse.json({ isAllowed: false });
+    userId = user.id;
+  }
 
   const data = await db.$queryRaw<Project[]>`SELECT "userId" FROM "Project" WHERE id = ${projectId}`;
   if (!data.length) return NextResponse.json({ isAllowed: false });
   if (data.length > 1) throw new Error("More than one project found with the same ID");
 
-  const isAllowed = data[0].userId === user?.id;
+  const isAllowed = data[0].userId === userId;
 
   return NextResponse.json({ isAllowed });
 };
