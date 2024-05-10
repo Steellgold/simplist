8a0a66923a43cdf56316446a11ae6146f3441317
Gaëtan H <steellgold0@gmail.com>
2024-05-10 15:12:53 +0200

    Add isDanger prop to CustomCard component

--- src/lib/components/ui/custom-card.tsx
+++ src/lib/components/ui/custom-card.tsx
@@ -7,7 +7,8 @@ export const CustomCard: Component<PropsWithChildren & {
   className?: string,
   active?: boolean,
   noHover?: boolean,
-}> = ({ children, className, active = false, noHover = false }) => {
+  isDanger?: boolean
+}> = ({ children, className, active = false, noHover = false, isDanger = false }) => {
   return (
     <Card className={cn(
       "h-full border-[2px] rounded-md",
@@ -17,7 +18,10 @@ export const CustomCard: Component<PropsWithChildren & {
         "hover:border-[#1a1a1a] hover:bg-[#161616]": active && !noHover,
 
         "bg-[#161616] border-[#1a1a1a]" : !active,
-        "hover:bg-[#1a1a1a] hover:border-[#2b2b2b]" : !active && !noHover
+        "hover:bg-[#1a1a1a] hover:border-[#2b2b2b]" : !active && !noHover,
+
+        "border-destructive/55": isDanger,
+        "bg-destructive/40": isDanger
       }
     )}>
       {children}
