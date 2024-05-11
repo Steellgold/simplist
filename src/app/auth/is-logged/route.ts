c7dcde4eee59acd80a487c217419f91750cc9ba2
Gaëtan H <steellgold0@gmail.com>
2024-05-11 12:02:30 +0200

    chore: Refactor getUser function in is-logged route

--- src/app/auth/is-logged/route.ts
+++ src/app/auth/is-logged/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async(): Promise<NextResponse> => {
   const supabase = createClient();
-  const { data: user, error } = await supabase.auth.getUser();
+  const { data: { user }, error } = await supabase.auth.getUser();
 
   if (!user || error) return NextResponse.json({ isLogged: false });
   return NextResponse.json({ isLogged: true });
