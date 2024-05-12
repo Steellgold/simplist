21e555e604042b2295fae15c4e6b280e38463cb9
Gaëtan H <gaetanhus@gmail.com>
2024-05-12 23:07:18 +0200

    refactor: Add fromDate, toDate and type param

--- src/app/api/user/projects/post/route.ts
+++ src/app/api/user/projects/post/route.ts
@@ -5,8 +5,10 @@ import { createClient } from "@/utils/supabase/server";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import type { Analytics } from "./analytics.type";
-import { BrowserSchema, CitiesSchema, CountriesSchema, DeviceSchema, OsSchema, RegionsSchema } from "./analytics.type";
+import { BrowserSchema, CitiesSchema, CountriesSchema, DeviceSchema, OsSchema, RegionsSchema, RequestsSchema } from "./analytics.type";
 import { tinybirdRequest } from "../../../tinybird.utils";
+import { dayJS } from "@/dayjs/day-js";
+import { requestsProcessor } from "@/utils/analytics";
 
 
 export const GET = async({ nextUrl }: NextRequest): Promise<NextResponse> => {
@@ -19,6 +21,10 @@ export const GET = async({ nextUrl }: NextRequest): Promise<NextResponse> => {
   const postId = params.get("postId");
   if (!projectId) return NextResponse.json({ error: "Project ID is required", projects: null }, { status: 400 });
   if (!postId) return NextResponse.json({ error: "Post ID is required", projects: null }, { status: 400 });
+
+  const fromDate = params.get("fromDate") ?? dayJS().format("YYYY-MM-DD");
+  const toDate = params.get("toDate") ?? dayJS().format("YYYY-MM-DD");
+  const type = params.get("type") as "day" | "hour" | "month" | "year" ?? "day";
 
   const post = await db.post.findUnique({
     where: {
@@ -64,29 +70,37 @@ export const GET = async({ nextUrl }: NextRequest): Promise<NextResponse> => {
     OSs: []
   };
 
-  const citiesSchema = CitiesSchema.safeParse(await tinybirdRequest("cities", { projectId, postId }, env.TINYBIRD_BEARER_TOKEN));
+  const citiesSchema = CitiesSchema.safeParse(await tinybirdRequest("cities", { projectId, postId, fromDate, toDate }, env.TINYBIRD_BEARER_TOKEN));
   if (!citiesSchema.success) analytics.cities = [];
   else analytics.cities = citiesSchema.data.data;
 
-  const countriesSchema = CountriesSchema.safeParse(await tinybirdRequest("countries", { projectId, postId }, env.TINYBIRD_BEARER_TOKEN));
+  const countriesSchema = CountriesSchema.safeParse(
+    await tinybirdRequest("countries", { projectId, postId, fromDate, toDate }, env.TINYBIRD_BEARER_TOKEN)
+  );
   if (!countriesSchema.success) analytics.countries = [];
   else analytics.countries = countriesSchema.data.data;
 
-  const regionsSchema = RegionsSchema.safeParse(await tinybirdRequest("regions", { projectId, postId }, env.TINYBIRD_BEARER_TOKEN));
+  const regionsSchema = RegionsSchema.safeParse(await tinybirdRequest("regions", { projectId, postId, fromDate, toDate }, env.TINYBIRD_BEARER_TOKEN));
   if (!regionsSchema.success) analytics.regions = [];
   else analytics.regions = regionsSchema.data.data;
 
-  const deviceSchema = DeviceSchema.safeParse(await tinybirdRequest("ua_device", { projectId, postId }, env.TINYBIRD_BEARER_TOKEN));
+  const deviceSchema = DeviceSchema.safeParse(await tinybirdRequest("ua_device", { projectId, postId, fromDate, toDate }, env.TINYBIRD_BEARER_TOKEN));
   if (!deviceSchema.success) analytics.devices = [];
   else analytics.devices = deviceSchema.data.data;
 
-  const browserSchema = BrowserSchema.safeParse(await tinybirdRequest("ua_browser", { projectId, postId }, env.TINYBIRD_BEARER_TOKEN));
+  const browserSchema = BrowserSchema.safeParse(
+    await tinybirdRequest("ua_browser", { projectId, postId, fromDate, toDate }, env.TINYBIRD_BEARER_TOKEN)
+  );
   if (!browserSchema.success) analytics.browsers = [];
   else analytics.browsers = browserSchema.data.data;
 
-  const osSchema = OsSchema.safeParse(await tinybirdRequest("ua_os", { projectId, postId }, env.TINYBIRD_BEARER_TOKEN));
+  const osSchema = OsSchema.safeParse(await tinybirdRequest("ua_os", { projectId, postId, fromDate, toDate }, env.TINYBIRD_BEARER_TOKEN));
   if (!osSchema.success) analytics.OSs = [];
   else analytics.OSs = osSchema.data.data;
 
-  return NextResponse.json({ post, analytics }, { status: 200 });
+
+  const graph = RequestsSchema.safeParse(await tinybirdRequest("post_calls", { projectId, postId, fromDate, toDate }, env.TINYBIRD_BEARER_TOKEN));
+  if (!graph.success) return NextResponse.json({ error: "Error fetching data", post, analytics, graph: null, type }, { status: 500 });
+
+  return NextResponse.json({ post, analytics, requests: requestsProcessor(graph.data.data, type) });
 };
