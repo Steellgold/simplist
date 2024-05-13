ccc92d24ed1fc629eaabb8dde6ad4633ae432436
Gaëtan H <gaetanhus@gmail.com>
2024-05-13 23:40:50 +0200

    refactor: Update page.tsx

---
+++ src/app/[id]/posts/[postId]/content/editor.tsx
@@ -0,0 +1,273 @@
+"use client";
+
+import { useEffect, useRef, useState, type ReactElement } from "react";
+import { CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { CustomCard } from "@/components/ui/custom-card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Bold, ImageIcon, Italic, Link, Sparkles, Strikethrough, Upload } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Badge } from "@/components/ui/badge";
+
+type EditorProps = {
+  title: string;
+};
+
+export const Editor = ({ title }: EditorProps): ReactElement => {
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+  const [selection, setSelection] = useState<string | null>(null);
+
+  const [imageURL, setImageURL] = useState<string | null>(null);
+  const [imageAlt, setImageAlt] = useState<string | null>(null);
+
+  const [linkURL, setLinkURL] = useState<string | null>(null);
+
+  useEffect(() => {
+    const handleSelection = (): void => {
+      if (contentRef.current) {
+        const selection = window.getSelection()?.toString();
+        if (selection) setSelection(selection);
+        else setSelection(null);
+      }
+    };
+
+    const content = contentRef.current;
+    if (content) {
+      content.addEventListener("mouseup", handleSelection);
+      content.addEventListener("keyup", handleSelection);
+      content.addEventListener("touchend", handleSelection);
+    }
+
+    return () => {
+      if (content) {
+        content.removeEventListener("mouseup", handleSelection);
+        content.removeEventListener("keyup", handleSelection);
+        content.removeEventListener("touchend", handleSelection);
+
+        setSelection(null);
+      }
+    };
+  }, []);
+
+  const isSelectionEmpty = !selection || selection.length === 0;
+
+  return (
+    <main className="grid items-start gap-4 mt-3">
+      <div className="max-w-full">
+        <div className="grid gap-4 lg:grid-cols-3">
+          <div className="grid auto-rows-max items-start gap-4 lg:col-span-2">
+            <CustomCard noHover>
+              <CardHeader>
+                <CardTitle>Title</CardTitle>
+                <CardDescription>Enter the title of your blog post. It should be catchy and reflect the content of your post.</CardDescription>
+              </CardHeader>
+
+              <CardContent className="flex items-center flex-col sm:flex-row gap-2">
+                <Input placeholder="Enter the title of your blog post" value={title} />
+                <Button className="w-full sm:w-auto" variant={"ai"}>Assistant <Sparkles className="h-4 w-4 ml-1.5" strokeWidth={1} /></Button>
+              </CardContent>
+            </CustomCard>
+
+            <CustomCard noHover>
+              <CardHeader>
+                <CardTitle>Excerpt</CardTitle>
+                <CardDescription>Write a brief description that grabs attention and makes readers want to read the entire article.</CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <Textarea placeholder="Enter the excerpt of your blog post here" className="w-full h-24 p-3 resize-none text-white rounded-md" />
+              </CardContent>
+            </CustomCard>
+
+            <CustomCard noHover>
+              <CardHeader>
+                <CardTitle>Content</CardTitle>
+                <CardDescription>Enter the content of your blog post. You can use markdown to format your text.</CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <div className="flex items-center gap-1.5 mb-2">
+                  <Button variant="inputStyle" disabled={isSelectionEmpty} onClick={() => {
+                    if (contentRef.current) {
+                      const text = contentRef.current.value;
+                      const selectionStart = contentRef.current.selectionStart;
+                      const selectionEnd = contentRef.current.selectionEnd;
+
+                      const newText = `${text.slice(0, selectionStart)}*${selection}*${text.slice(selectionEnd)}`;
+                      contentRef.current.value = newText;
+                    }
+                  }}>
+                    <Italic className="h-4 w-4" />
+                  </Button>
+
+                  <Button variant="inputStyle" disabled={isSelectionEmpty} onClick={() => {
+                    if (contentRef.current) {
+                      const text = contentRef.current.value;
+                      const selectionStart = contentRef.current.selectionStart;
+                      const selectionEnd = contentRef.current.selectionEnd;
+
+                      const newText = `${text.slice(0, selectionStart)}**${selection}**${text.slice(selectionEnd)}`;
+                      contentRef.current.value = newText;
+                    }
+                  }}>
+                    <Bold className="h-4 w-4" />
+                  </Button>
+
+                  <Button variant="inputStyle" disabled={isSelectionEmpty} onClick={() => {
+                    if (contentRef.current) {
+                      const text = contentRef.current.value;
+                      const selectionStart = contentRef.current.selectionStart;
+                      const selectionEnd = contentRef.current.selectionEnd;
+
+                      const newText = `${text.slice(0, selectionStart)}~~${selection}~~${text.slice(selectionEnd)}`;
+                      contentRef.current.value = newText;
+                    }
+                  }}>
+                    <Strikethrough className="h-4 w-4" />
+                  </Button>
+
+                  <Dialog>
+                    <DialogTrigger>
+                      <Button variant="inputStyle" disabled={!isSelectionEmpty}>
+                        <ImageIcon className="h-4 w-4" />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Insert Image</DialogTitle>
+                        <DialogDescription>Upload or insert a link to an image that you want to include in your blog post.</DialogDescription>
+                      </DialogHeader>
+
+                      <Input placeholder="Enter the alt text of the image" value={imageAlt ?? ""} onChange={(e) => setImageAlt(e.target.value)} />
+                      <Input placeholder="Enter the URL of the image" value={imageURL ?? ""} onChange={(e) => setImageURL(e.target.value)} />
+                      <Button disabled variant="inputStyle">Upload Image&nbsp;&nbsp;<Badge variant={"outline"}>Soon</Badge></Button>
+
+                      <DialogFooter>
+                        <Button variant="outline" onClick={() => {}}>Cancel</Button>
+                        <Button
+                          disabled={!imageURL || !imageAlt}
+                          onClick={() => {
+                            if (contentRef.current) {
+                              const text = contentRef.current.value;
+                              const selectionStart = contentRef.current.selectionStart;
+                              const selectionEnd = contentRef.current.selectionEnd;
+
+                              const newText = `${text.slice(0, selectionStart)}![${imageAlt}](${imageURL})${text.slice(selectionEnd)}`;
+                              contentRef.current.value = newText;
+                              setImageURL(null);
+                              setImageAlt(null);
+                            }
+                          }}>Insert Image</Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+
+                  {/* <Button variant="inputStyle" disabled={isSelectionEmpty}>
+                    <Link className="h-4 w-4" />
+                  </Button> */}
+
+                  <Dialog>
+                    <DialogTrigger>
+                      <Button variant="inputStyle" disabled={isSelectionEmpty}>
+                        <Link className="h-4 w-4" />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Insert Link</DialogTitle>
+                        <DialogDescription>Insert a link to a website or another blog post.</DialogDescription>
+                      </DialogHeader>
+
+                      <Input placeholder="Enter the URL of the link" value={linkURL ?? ""} onChange={(e) => setLinkURL(e.target.value)} />
+
+                      <DialogFooter>
+                        <Button variant="outline" onClick={() => {}}>Cancel</Button>
+                        <Button
+                          disabled={!linkURL || !selection}
+                          onClick={() => {
+                            if (contentRef.current) {
+                              const text = contentRef.current.value;
+                              const selectionStart = contentRef.current.selectionStart;
+                              const selectionEnd = contentRef.current.selectionEnd;
+
+                              const newText = `${text.slice(0, selectionStart)}[${selection}](${linkURL})${text.slice(selectionEnd)}`;
+                              contentRef.current.value = newText;
+                              setLinkURL(null);
+                            }
+                          }}>Insert Link</Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+
+                  <Separator orientation="vertical" className="h-4" />
+
+                  {/* <Button variant="inputStyle" disabled={isSelectionEmpty}>
+                    H<span className="text-[10px]">?</span>
+                  </Button> */}
+                  <Select defaultValue="h1">
+                    <SelectTrigger className="w-[100px]">
+                      <SelectValue>Heading</SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="h1">H1</SelectItem>
+                      <SelectItem value="h2">H2</SelectItem>
+                      <SelectItem value="h3">H3</SelectItem>
+                      <SelectItem value="h4">H4</SelectItem>
+                      <SelectItem value="h5">H5</SelectItem>
+                      <SelectItem value="h6">H6</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                <Textarea
+                  ref={contentRef}
+                  className="h-96 p-3"
+                  placeholder="Enter the content of your blog post"
+                />
+              </CardContent>
+            </CustomCard>
+          </div>
+
+          <div className="grid auto-rows-max gap-4">
+            <CustomCard noHover>
+              <CardHeader>
+                <CardTitle>Visibility</CardTitle>
+                <CardDescription>Choose the visibility of your blog post. You can choose to make it published or drafted.</CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <Select defaultValue="published">
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select the visibility of your blog post" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="published">Published</SelectItem>
+                    <SelectItem value="drafted">Drafted</SelectItem>
+                  </SelectContent>
+                </Select>
+              </CardContent>
+            </CustomCard>
+
+            <CustomCard noHover>
+              <CardHeader>
+                <CardTitle>Banner Image</CardTitle>
+                <CardDescription>Upload a banner image for your blog post. It will be displayed at the top of your post.</CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <input type="file" className="opacity-0 absolute z-[-1]" id="file-banner-input" />
+                <Button size="sm" className="gap-1 w-full" onClick={() => document?.getElementById("file-banner-input")?.click()}>
+                  <Upload className="h-3.5 w-3.5" />
+                    Upload Image
+                </Button>
+              </CardContent>
+            </CustomCard>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
