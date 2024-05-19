5d4c11691eee8b3e8d440b8d71d4332ae64c64d9
Gaëtan H <gaetanhus@gmail.com>
2024-05-19 23:02:58 +0200

    refactor: Update MarkdownButtons component to use Lucide icons and improve code structure

--- src/app/[id]/posts/[postId]/content/editor/index.tsx
+++ src/app/[id]/posts/[postId]/content/editor/index.tsx
@@ -1,21 +1,51 @@
 import type { Component } from "@/components/utils/component";
-import { ButtonBold } from "./button.bold";
-import { ButtonItalic } from "./button.italic";
-import { ButtonStrikethrough } from "./button.strikethrough";
+import type { ReactElement, RefObject } from "react";
+import { Bold, Italic, Strikethrough } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type MarkdownButtonsType = {
-  contentRef: React.RefObject<HTMLTextAreaElement>;
+  contentRef: RefObject<HTMLTextAreaElement>;
   selection: string;
   isDisabled: boolean;
   onContentChange: (content: string) => void;
+  type: "bold" | "italic" | "strikethrough";
 };
 
-export const MarkdownButtons: Component<MarkdownButtonsType> = ({ contentRef, selection, isDisabled, onContentChange }) => {
+export const markdownProcessor = ({ contentRef, selection, isDisabled, onContentChange, type }: MarkdownButtonsType): boolean => {
+  if (isDisabled) return false;
+  if (!contentRef.current) return false;
+  if (!selection) return false;
+
+  const text = contentRef.current.value;
+  const selectionStart = contentRef.current.selectionStart;
+  const selectionEnd = contentRef.current.selectionEnd;
+
+  let newText = text;
+  switch (type) {
+    case "bold":
+      newText = `${text.slice(0, selectionStart)}**${selection}**${text.slice(selectionEnd)}`;
+      break;
+    case "italic":
+      newText = `${text.slice(0, selectionStart)}*${selection}*${text.slice(selectionEnd)}`;
+      break;
+    case "strikethrough":
+      newText = `${text.slice(0, selectionStart)}~~${selection}~~${text.slice(selectionEnd)}`;
+      break;
+  }
+
+  onContentChange(newText);
+  return true;
+};
+
+export const MarkdownButton: Component<MarkdownButtonsType> = ({ contentRef, selection, isDisabled, onContentChange, type }): ReactElement => {
   return (
-    <>
-      <ButtonBold contentRef={contentRef} selection={selection} isDisabled={isDisabled} onContentChange={onContentChange} />
-      <ButtonItalic contentRef={contentRef} selection={selection} isDisabled={isDisabled} onContentChange={onContentChange} />
-      <ButtonStrikethrough contentRef={contentRef} selection={selection} isDisabled={isDisabled} onContentChange={onContentChange} />
-    </>
+    <Button
+      variant="inputStyle"
+      disabled={isDisabled}
+      onClick={() => markdownProcessor({ contentRef, selection, isDisabled, onContentChange, type })}>
+      {type === "bold" && <Bold className="h-4 w-4" />}
+      {type === "italic" && <Italic className="h-4 w-4" />}
+      {type === "strikethrough" && <Strikethrough className="h-4 w-4" />}
+    </Button>
   );
 };
