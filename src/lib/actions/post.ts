ebffc7f3922ff88aa4d128a208e536ee52833c5a
Gaëtan H <gaetanhus@gmail.com>
2024-05-20 16:59:35 +0200

    refactor: Update updatePost function to use Prisma raw queries and add redirect after updating post

--- src/lib/actions/post.ts
+++ src/lib/actions/post.ts
@@ -6,6 +6,7 @@ import { db } from "@/utils/db/prisma";
 import { createClient } from "@/utils/supabase/server";
 import type { Post } from "@prisma/client";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
 export const updatePost = async(id: string, data: z.infer<typeof PostSchema>): Promise<{ success: boolean; message?: string }> => {
@@ -13,16 +14,16 @@ export const updatePost = async(id: string, data: z.infer<typeof PostSchema>): P
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) throw new Error("User not found");
 
-  const response = await fetch(`${env.NEXT_PUBLIC_APP_URL}/auth/is-allowed?projectId=${id}&userId=${user?.id}`);
+  const response = await fetch(`${env.NEXT_PUBLIC_APP_URL}/auth/is-allowed?projectId=${data.projectId}&userId=${user?.id}`);
   const schema = z.object({ isAllowed: z.boolean() }).safeParse(await response.json());
 
   if (!schema.success) return { success: false, message: "Failed to check if you are allowed to update this project" };
   if (!schema.data.isAllowed) return { success: false, message: "You are not allowed to update this project" };
 
-  const post = await db.$queryRaw<Post | null>`SELECT * FROM Post WHERE id = ${id}`;
+  const post = await db.$queryRaw<Post | null>`SELECT * FROM "Post" WHERE id = ${id}`;
   if (!post) return { success: false, message: "Post not found" };
 
-  await db.post.update({
+  const postData = await db.post.update({
     where: { id },
     data: {
       title: data.title,
@@ -32,7 +33,6 @@ export const updatePost = async(id: string, data: z.infer<typeof PostSchema>): P
     }
   });
 
-  revalidatePath(`/${post.projectId}/posts`);
-
-  return { success: true };
+  revalidatePath(`/${postData.projectId}/posts`);
+  redirect(`/${postData.projectId}/posts`);
 };
