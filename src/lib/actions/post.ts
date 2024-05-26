c65a928ea3e6db54c864f03d08ef77fb75a0271e
Gaëtan H <gaetanhus@gmail.com>
2024-05-26 17:32:26 +0200

    refactor: Add createPost action for creating new posts

--- src/lib/actions/post.ts
+++ src/lib/actions/post.ts
@@ -38,6 +38,36 @@ export const updatePost = async(id: string, data: z.infer<typeof PostSchema>): P
   redirect(`/${postData.projectId}/posts`);
 };
 
+export const createPost = async(data: z.infer<typeof PostSchema>): Promise<{ success: boolean; message?: string }> => {
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+  if (!user) throw new Error("User not found");
+
+  const response = await fetch(`${env.NEXT_PUBLIC_APP_URL}/auth/is-allowed?projectId=${data.projectId}&userId=${user?.id}`);
+  const schema = z.object({ isAllowed: z.boolean() }).safeParse(await response.json());
+
+  if (!schema.success) return { success: false, message: "Failed to check if you are allowed to create a new post" };
+  if (!schema.data.isAllowed) return { success: false, message: "You are not allowed to create a new post" };
+
+  const postData = await db.post.create({
+    data: {
+      content: data.content,
+      excerpt: data.excerpt,
+      title: data.title,
+      status: data.status,
+      banner: data.banner,
+      slug: data.slug,
+      author: { connect: { id: user.id } },
+      project: { connect: { id: data.projectId } }
+    }
+  });
+
+  if (!postData) return { success: false, message: "Failed to create a new post" };
+
+  revalidatePath(`/${postData.projectId}/posts`);
+  redirect(`/${postData.projectId}/posts/${postData.id}`);
+};
+
 export const deletePost = async(id: string, projectId: string): Promise<{ success: boolean; message?: string }> => {
   const supabase = createClient();
   const { data: { user } } = await supabase.auth.getUser();
