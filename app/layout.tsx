5eefbc18d9a28079e79d2e45a5b9df5aad01795e
Gaëtan H <gaetanhus@gmail.com>
2024-10-17 18:40:00 +0200

    feat(layout): integrate ThemeProvider and Toaster components for improved UI management feat(theme-provider): create ThemeProvider component to manage theme context feat(theme-switcher): add ThemeSwitcher component for toggling between light and dark themes feat(component): define reusable component types for better type safety in components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +27,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <Toaster />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
