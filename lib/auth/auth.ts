7c9a8d088e8b38f389a2103be910684e239436ea
Gaëtan H <gaetanhus@gmail.com>
2024-10-17 16:01:26 +0200

    feat(auth): comment out social provider configuration for future use feat(deps): add dayjs with various plugins for enhanced date manipulation

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -13,20 +13,20 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true
     },
-    socialProviders: {
-        discord: {
-            clientId: process.env.DISCORD_CLIENT_ID as string,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET as string
-        },
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        },
-        github: {
-            clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-        }
-    },
+    // socialProviders: {
+    //     discord: {
+    //         clientId: process.env.DISCORD_CLIENT_ID as string,
+    //         clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+    //     },
+    //     google: {
+    //         clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    //     },
+    //     github: {
+    //         clientId: process.env.GITHUB_CLIENT_ID as string,
+    //         clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+    //     }
+    // },
     plugins: [
         passkey(),
         organization(),
