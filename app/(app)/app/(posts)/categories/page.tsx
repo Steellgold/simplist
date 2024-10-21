ecdde7afa800c565c31072462a193d21f8132475
Gaëtan H <gaetanhus@gmail.com>
2024-10-21 19:55:43 +0200

    feat(posts): update headings for categories and tags pages

--- app/(app)/app/(posts)/categories/page.tsx
+++ app/(app)/app/(posts)/categories/page.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react";
 const Page = (): ReactElement => {
   return (
     <div className="container mx-auto p-4 mt-16">
-      <h1>Posts</h1>
+      <h1>Categories</h1>
     </div>
   )
 }
