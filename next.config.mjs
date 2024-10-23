b29ff75b7e7a1779faa220824a1b6f277c2a9dde
Gaëtan H <gaetanhus@gmail.com>
2024-10-23 21:05:38 +0200

    chore: update Next.js and related dependencies to maintain compatibility and improve performance style(globals.css): add newline at the end of the file for consistency feat(next.config.mjs): add remotePatterns for images to allow loading from placehold.co

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "placehold.co", protocol: "https" }
+    ]
+  }
+};
 
 export default nextConfig;
