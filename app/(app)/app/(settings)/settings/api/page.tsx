f3e84b7a49e294f8801a5592e1fd66884fe2f457
Gaëtan H <gaetanhus@gmail.com>
2024-10-24 17:52:53 +0200

    feat(settings): add new pages for API Keys, Integrations, and update existing pages to use type imports for ReactElement
    style(settings): format existing page components to use consistent syntax and improve readability

--- app/(app)/app/(settings)/settings/api/page.tsx
+++ app/(app)/app/(settings)/settings/api/page.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from "react";
 const Page = (): ReactElement => {
   return (
     <div className="container mx-auto p-4 mt-16">
-      <h1>Settings</h1>
+      <h1>API Keys</h1>
     </div>
   );
 };
--- app/(app)/app/(settings)/settings/integrations/page.tsx
+++ app/(app)/app/(settings)/settings/integrations/page.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from "react";
 const Page = (): ReactElement => {
   return (
     <div className="container mx-auto p-4 mt-16">
-      <h1>Settings</h1>
+      <h1>Integrations</h1>
     </div>
   );
 };
