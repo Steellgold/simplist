b198ec92c7a84e94a31e448e127e3106dfae49f0
Gaëtan H <me@gaetanhus.fr>
2025-02-14 13:19:15 +0100

    Remove project files

--- src/lib/utils/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-export const createClient = (): ReturnType<typeof createBrowserClient> => createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
