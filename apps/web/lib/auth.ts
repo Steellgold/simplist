8c5b5a02e2bde0bbcb61aa23b5351d9ebca2ca4d
Gaëtan H <gaetanhus@gmail.com>
2025-02-16 15:29:31 +0100

    feat(auth): update PrismaClient to use edge version with accelerate extension

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,9 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge"
+import { withAccelerate } from "@prisma/extension-accelerate"
 import { organization, username } from "better-auth/plugins";
  
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 export const auth = betterAuth({
   emailAndPassword: {
