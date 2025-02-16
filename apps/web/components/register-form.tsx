516364cdb4e1d24b0cf2026c303a2e19c693ce15
Gaëtan H <gaetanhus@gmail.com>
2025-02-16 17:02:44 +0100

    feat(ui): enhance register form with validation, loading state, and improved user feedback

--- apps/web/components/register-form.tsx
+++ apps/web/components/register-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -11,12 +13,32 @@ import { Component } from "@workspace/ui/components/utils/component"
 import { Button } from "@workspace/ui/components/button"
 import { cn } from "@workspace/ui/lib/utils"
 import { PasswordInputRequirements } from "@workspace/ui/components/requirements-input-password"
+import { useToast } from "@workspace/ui/hooks/use-toast";
 import { OAuthsButtons } from "./oauths-buttons"
+import { z } from "zod"
+import { ToastAction } from "@workspace/ui/components/toast";
+import { useState } from "react";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+
+const schema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+
+  email: z.string().email(),
+  password: z.string()
+})
 
 export const RegisterForm: Component<React.ComponentPropsWithoutRef<"div">> = ({
   className,
   ...props
 }) => {
+  const { toast } = useToast();
+
+	const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -25,28 +47,89 @@ export const RegisterForm: Component<React.ComponentPropsWithoutRef<"div">> = ({
           <CardDescription>Sign up with your email or use a social account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={async(e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+
+            const formData = Object.fromEntries(new FormData(e.currentTarget).entries());
+            const result = schema.safeParse(formData);
+
+            if (!result.success) {
+              toast({
+                title: "Data validation error",
+                description: "Please check the form fields and try again",
+                action: <ToastAction altText="Try again">Try again</ToastAction>,      
+              })
+              return;
+            }
+
+            await authClient.signUp.email({
+              email: result.data.email,
+              password: result.data.password,
+              name: `${result.data.firstName} ${result.data.lastName}`,
+              callbackURL: "/",
+              fetchOptions: {
+                onResponse: () => {
+                  setLoading(false);
+                },
+                onRequest: () => {
+                  setLoading(true);
+                },
+                onError: (ctx) => {
+                  toast({
+                    title: "Error",
+                    description: ctx.error.message,
+                    action: <ToastAction altText="Close">Close</ToastAction>,
+                  })
+                },
+                onSuccess: async () => {
+                  toast({
+                    title: "Account created",
+                    description: "Your account has been created successfully 🎉",
+                    action: <ToastAction altText="Close">Close</ToastAction>,
+                  });
+
+                  router.push("/");
+                },
+              },
+            });
+          }}>
             <div className="grid gap-6">
               <OAuthsButtons />
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">Or sign up with email</span>
               </div>
               <div className="grid gap-3">
-                <div className="grid gap-2">
-                  <Label htmlFor="name">Full Name</Label>
-                  <Input id="name" type="text" placeholder="John Doe" required />
+                <div className="flex gap-3">
+                  <div className="grid gap-2">
+                    <Label htmlFor="firstName">First Name</Label>
+                    <Input id="firstName" type="text" placeholder="John" required name="firstName" />
+                  </div>
+
+                  <div className="grid gap-2">
+                    <Label htmlFor="lastName">Last Name</Label>
+                    <Input id="lastName" type="text" placeholder="Doe" required name="lastName" />
+                  </div>
                 </div>
                 <div className="grid gap-2">
                   <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="m@example.com" required />
+                  <Input id="email" type="email" placeholder="m@example.com" required name="email" />
                 </div>
                 
                 <PasswordInputRequirements  />
                 
-                <Button type="submit" className="w-full">
-                  Create Account
+                <Button
+                  type="submit"
+                  className="w-full"
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <Loader size={16} className="animate-spin" />
+                  ) : (
+                    "Create an account"
+                  )}
                 </Button>
               </div>
+
               <div className="text-center text-sm">
                 Already have an account?{" "}
                 <a href="/auth" className="underline underline-offset-4">
