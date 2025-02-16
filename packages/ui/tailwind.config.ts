d01e88189bf42b66b9b1e17e6c9ccf9ea3b82de8
Gaëtan H <gaetanhus@gmail.com>
2025-02-16 17:02:22 +0100

    feat(ui): update skeleton component styles and add skeleton color variables for improved theming

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -13,63 +13,67 @@ const config = {
   	extend: {
   		fontFamily: {
   			sans: [
-  				'var(--font-sans)',
-                    ...fontFamily.sans
-                ],
+  				"var(--font-sans)",
+					...fontFamily.sans
+        ],
   			mono: [
-  				'var(--font-mono)',
-                    ...fontFamily.mono
-                ]
+					"var(--font-mono)",
+          ...fontFamily.mono
+        ]
   		},
   		colors: {
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
+  			border: "hsl(var(--border))",
+  			input: "hsl(var(--input))",
+  			ring: "hsl(var(--ring))",
+  			background: "hsl(var(--background))",
+  			foreground: "hsl(var(--foreground))",
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  				DEFAULT: "hsl(var(--primary))",
+  				foreground: "hsl(var(--primary-foreground))"
   			},
+				skeleton: {
+					DEFAULT: "hsl(var(--skeleton))",
+					foreground: "hsl(var(--skeleton-foreground))"
+				},
   			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
+  				DEFAULT: "hsl(var(--secondary))",
+  				foreground: "hsl(var(--secondary-foreground))"
   			},
   			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
+  				DEFAULT: "hsl(var(--destructive))",
+  				foreground: "hsl(var(--destructive-foreground))"
   			},
   			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
+  				DEFAULT: "hsl(var(--muted))",
+  				foreground: "hsl(var(--muted-foreground))"
   			},
   			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
+  				DEFAULT: "hsl(var(--accent))",
+  				foreground: "hsl(var(--accent-foreground))"
   			},
   			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
+  				DEFAULT: "hsl(var(--popover))",
+  				foreground: "hsl(var(--popover-foreground))"
   			},
   			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
+  				DEFAULT: "hsl(var(--card))",
+  				foreground: "hsl(var(--card-foreground))"
   			},
   			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
+  				DEFAULT: "hsl(var(--sidebar-background))",
+  				foreground: "hsl(var(--sidebar-foreground))",
+  				primary: "hsl(var(--sidebar-primary))",
+  				"primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+  				accent: "hsl(var(--sidebar-accent))",
+  				"accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+  				border: "hsl(var(--sidebar-border))",
+  				ring: "hsl(var(--sidebar-ring))"
   			}
   		},
   		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
+  			lg: "var(--radius)",
+  			md: "calc(var(--radius) - 2px)",
+  			sm: "calc(var(--radius) - 4px)"
   		}
   	}
   },
