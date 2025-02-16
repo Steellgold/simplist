e90a488019c908f7ea2a1c5654c2cee9f6616b39
Gaëtan H <gaetanhus@gmail.com>
2025-02-16 13:52:27 +0100

    refactor(login-page): remove unused RegisterForm component for cleaner layout

--- apps/web/app/auth/page.tsx
+++ apps/web/app/auth/page.tsx
@@ -1,12 +1,10 @@
 import { LoginForm } from "@/components/login-form"
-import { RegisterForm } from "@/components/register-form";
 
 const LoginPage = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <LoginForm />
-        <RegisterForm />
       </div>
     </div>
   )
