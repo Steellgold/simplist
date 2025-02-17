0f3642b63b8e38567c8c180e1bd6ad92602331a9
Gaëtan H <me@gaetanhus.fr>
2025-02-17 16:03:00 +0100

    feat(page): update main page layout and add user settings component

--- apps/web/app/(app)/page.tsx
+++ apps/web/app/(app)/page.tsx
@@ -2,9 +2,14 @@ import { ReactElement } from "react";
 
 const Page = (): ReactElement => {
   return (
-    <div className="bg-gray-100">
-      Page
-    </div>
+    <>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    </>
   )
 }
 
---
+++ apps/web/app/(app)/user/page.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const UserSettings = (): ReactElement => {
+  return <div>User Settings</div>;
+};
+
+export default UserSettings;
