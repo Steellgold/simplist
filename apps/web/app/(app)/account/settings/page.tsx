1cb61e1074fe251671ddcdbeade1551f162710c6
Gaëtan H <me@gaetanhus.fr>
2025-02-18 11:44:06 +0100

    feat(account): add Account Settings and Home pages

---
+++ apps/web/app/(app)/account/settings/page.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const AccountSettingsPage = (): ReactElement => {
+  return <div>Account Settings Page</div>;
+};
+
+export default AccountSettingsPage;
---
+++ apps/web/app/(app)/page.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const Home = (): ReactElement => {
+  return <div>Home</div>;
+};
+
+export default Home;
