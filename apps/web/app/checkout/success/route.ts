5149d043b45626e88942d41807408f197cbcbc91
Gaëtan H <gaetanhus@gmail.com>
2025-02-19 21:36:25 +0100

    feat(checkout/success): update GET response to return JSON format

--- apps/web/app/checkout/success/route.ts
+++ apps/web/app/checkout/success/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export const GET = (req: Request): NextResponse => {
-  return new Response("Hello, world!");
+export const GET = (): NextResponse => {
+  return NextResponse.json({ message: "Hello, World!" });
 };
