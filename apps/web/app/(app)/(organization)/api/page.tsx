52de7d46f7f81ef7693d1bfc6f1498f93527ef59
Gaëtan H <gaetanhus@gmail.com>
2025-02-23 21:35:51 +0100

    feat(organization-api): create OrganizationAPI component for displaying API information

---
+++ apps/web/app/(app)/(organization)/api/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const OrganizationAPI = (): ReactElement => {
+  return (
+    <div>
+      <h1>Organization API</h1>
+    </div>
+  );
+}
+
+export default OrganizationAPI;
