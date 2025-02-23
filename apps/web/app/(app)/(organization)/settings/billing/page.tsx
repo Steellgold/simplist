2555180b47f4346f4726e5ee08342b0e1c7823ec
Gaëtan H <gaetanhus@gmail.com>
2025-02-23 21:36:05 +0100

    feat(organization-billing): add OrganizationBilling component for displaying billing information

---
+++ apps/web/app/(app)/(organization)/settings/billing/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const OrganizationBilling = (): ReactElement => {
+  return (
+    <div>
+      <h1>Organization Billing</h1>
+    </div>
+  );
+}
+
+export default OrganizationBilling;
