e34264456a904490c09f1c1177d16f62230a7752
Gaëtan H <gaetanhus@gmail.com>
2025-02-23 21:36:00 +0100

    feat(organization-posts): add OrganizationPosts component for displaying organization posts

---
+++ apps/web/app/(app)/(organization)/posts/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const OrganizationPosts = (): ReactElement => {
+  return (
+    <div>
+      <h1>Organization Posts</h1>
+    </div>
+  );
+}
+
+export default OrganizationPosts;
