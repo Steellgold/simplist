58c9557272bd09c8f824e5589618299df659dc94
Gaëtan H <gaetanhus@gmail.com>
2025-02-23 21:35:57 +0100

    feat(organization-members): add OrganizationMembers component for displaying organization members

---
+++ apps/web/app/(app)/(organization)/members/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const OrganizationMembers = (): ReactElement => {
+  return (
+    <div>
+      <h1>Organization Members</h1>
+    </div>
+  );
+}
+
+export default OrganizationMembers;
