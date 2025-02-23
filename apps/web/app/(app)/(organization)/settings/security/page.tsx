87184a654649525ec16489d823af238e37c41f8f
Gaëtan H <gaetanhus@gmail.com>
2025-02-23 21:36:08 +0100

    feat(organization-settings): add OrganizationSettings component for managing organization settings

---
+++ apps/web/app/(app)/(organization)/settings/security/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const OrganizationSettings = (): ReactElement => {
+  return (
+    <div>
+      <h1>Organization Settings</h1>
+    </div>
+  );
+}
+
+export default OrganizationSettings;
