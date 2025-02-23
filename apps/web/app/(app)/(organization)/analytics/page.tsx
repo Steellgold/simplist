da371207412b8aa2e880fee388466880cbd3cb28
Gaëtan H <gaetanhus@gmail.com>
2025-02-23 21:35:34 +0100

    feat(organization-analytics): create OrganizationAnalytics component for displaying analytics

---
+++ apps/web/app/(app)/(organization)/analytics/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const OrganizationAnalytics = (): ReactElement => {
+  return (
+    <div>
+      <h1>Organization Analytics</h1>
+    </div>
+  );
+}
+
+export default OrganizationAnalytics;
